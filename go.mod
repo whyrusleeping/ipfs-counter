2407b5bb3b352a184332d7ea9859e686f743d2d8
Adin Schmahmann <adin.schmahmann@gmail.com>
2020-06-04 02:31:53 -0400

    use dht crawler

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/aschmahmann/dht-graph
 go 1.14
 
 require (
+	github.com/ipfs/go-log v1.0.4
 	github.com/libp2p/go-libp2p v0.8.2
 	github.com/libp2p/go-libp2p-core v0.5.4
 	github.com/libp2p/go-libp2p-kad-dht v0.8.2-0.20200601201404-b918f4221db7
-	github.com/libp2p/go-libp2p-kbucket v0.4.2
 	github.com/multiformats/go-multiaddr v0.2.1
+	go.uber.org/zap v1.14.1
 )
+
+replace github.com/libp2p/go-libp2p-kad-dht => ../../libp2p/go-libp2p-kad-dht
--- main.go
+++ main.go
@@ -2,18 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
+	logging "github.com/ipfs/go-log"
 	"os"
 	"time"
 
 	"github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p-core/protocol"
 
-	kaddht "github.com/libp2p/go-libp2p-kad-dht"
-	kbucket "github.com/libp2p/go-libp2p-kbucket"
+	"github.com/libp2p/go-libp2p-kad-dht/crawler"
 	"github.com/multiformats/go-multiaddr"
 )
 
@@ -26,15 +25,14 @@ func main() {
 	flag.Parse()
 
 	peerMap := make(map[peer.ID]map[peer.ID]struct{})
-	allPeers := make([]*peer.AddrInfo, 0, 1000)
-	allPeersSet := make(map[peer.ID]struct{})
+	failedPeers := make(map[peer.ID]error)
+
+	pd := make(map[peer.ID]*PeerData)
 
 	h, err := libp2p.New(ctx)
 	if err != nil {
 		panic(err)
 	}
-
-	dhtRPC := kaddht.NewProtocolMessenger(h, []protocol.ID{"/ipfs/kad/1.0.0"}, nil)
 
 	//"/dnsaddr/sjc-2.bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp"
 	v2Addr := "/ip4/139.178.89.189/tcp/4001/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp"
@@ -57,87 +55,81 @@ func main() {
 	}
 	h.Peerstore().AddAddrs(startingPeerInfo.ID, startingPeerInfo.Addrs, time.Hour)
 
-	queryQueue := make(chan peer.ID, 1)
-	queryResQueue := make(chan *queryResult, 1)
-
-	maxQueries := 10000
-	for i := 0; i < maxQueries; i++ {
-		go func() {
-			for {
-				select {
-				case p := <-queryQueue:
-					res := queryPeer(ctx, h, dhtRPC, p)
-					select {
-					case queryResQueue <- res:
-					case <-ctx.Done():
-						return
-					}
-				case <-ctx.Done():
-					return
-				}
-			}
-		}()
+	c, err := crawler.New(h, crawler.WithParallelism(1000))
+	if err != nil {
+		panic(err)
 	}
 
-	allPeers = append(allPeers, startingPeerInfo)
-	allPeersSet[startingPeerInfo.ID] = struct{}{}
+	logging.SetLogLevel("dht-crawler", "debug")
+	l := logging.Logger("crawlapp")
+	logging.SetLogLevel("crawlapp", "debug")
 
-	numPeersTotal := 1
-	numPeersQueried := 0
-	numOutstandingQueries := 0
-
-	var nextPeer peer.ID
-	var sendNextPeer chan peer.ID
-
-	for numPeersQueried < numPeersTotal || numOutstandingQueries != 0 {
-		if nextPeer == "" && numPeersQueried < numPeersTotal {
-			nextPeer = allPeers[numPeersQueried].ID
-		}
-
-		if nextPeer == "" {
-			sendNextPeer = nil
-		} else {
-			sendNextPeer = queryQueue
-		}
-
-		select {
-		case res := <-queryResQueue:
-			if res.err == nil {
-				fmt.Printf("peer %v had %d peers\n", res.peer, len(res.data))
-				if _, ok := peerMap[res.peer]; !ok {
-					rtPeers := make(map[peer.ID]struct{}, len(res.data))
-					for p := range res.data {
-						rtPeers[p] = struct{}{}
-					}
-					peerMap[res.peer] = rtPeers
-				} else {
-					panic("how?")
-				}
-
-				for p, ai := range res.data {
-					h.Peerstore().AddAddrs(p, ai.Addrs, time.Hour)
-					if _, ok := allPeersSet[p]; !ok {
-						allPeersSet[p] = struct{}{}
-						allPeers = append(allPeers, ai)
-						numPeersTotal++
-					}
-				}
+	short, c2 := context.WithTimeout(ctx, time.Hour*20)
+	defer c2()
+	c.Run(short, []*peer.AddrInfo{startingPeerInfo},
+		func(p peer.ID, rtPeers []*peer.AddrInfo) {
+			if _, ok := peerMap[p]; ok {
+				panic("should not hit this twice")
 			}
-			numOutstandingQueries--
-		case sendNextPeer <- nextPeer:
-			numOutstandingQueries++
-			numPeersQueried++
-			nextPeer = ""
-			fmt.Printf("starting %d out of %d\n", numPeersQueried, len(allPeers))
-		case <-ctx.Done():
-			return
-		}
-	}
+			rtPeerSet := make(map[peer.ID]struct{}, len(rtPeers))
+			for _, ai := range rtPeers {
+				rtPeerSet[ai.ID] = struct{}{}
+			}
+			peerMap[p] = rtPeerSet
 
-	OutputData(*out, *outJson, peerMap)
+			ua, err := h.Peerstore().Get(p, "AgentVersion")
+			if err != nil {
+				ua = ""
+			}
+			pv, err := h.Peerstore().Get(p, "ProtocolVersion")
+			if err != nil {
+				pv = ""
+			}
+			if _, ok := pd[p]; ok {
+				panic("nooo")
+			}
+			pd[p] = &PeerData{
+				Peer: p,
+				Success:         true,
+				Addrs:           h.Peerstore().Addrs(p),
+				UserAgent:       ua.(string),
+				ProtocolVersion: pv.(string),
+				RTPeers:         rtPeerSet,
+			}
+			l.Debugf("%d out of %d successful", len(peerMap), len(pd))
+		},
+		func(p peer.ID, err error) {
+			if _, ok := failedPeers[p]; ok {
+				panic("we hit this twice?")
+			}
+			failedPeers[p] = err
+			if _, ok := pd[p]; ok {
+				panic("nooo")
+			}
+			pd[p] = &PeerData{
+				Peer: p,
+				Success: false,
+				Addrs : h.Peerstore().Addrs(p),
+				Err : err.Error(),
+			}
+
+			l.Debugf("%d out of %d successful", len(peerMap), len(pd))
+		},
+	)
+
+	OutputData(*out, *outJson, peerMap, pd)
 }
 
-func OutputData(filePath, jsonFile string, peerMap map[peer.ID]map[peer.ID]struct{}) {
+type PeerData struct {
+	Peer                       peer.ID
+	Success                    bool
+	Addrs                      []multiaddr.Multiaddr
+	UserAgent, ProtocolVersion string
+	RTPeers                    map[peer.ID]struct{}
+	Err                        string
+}
+
+func OutputData(filePath, jsonFile string, peerMap map[peer.ID]map[peer.ID]struct{}, pd map[peer.ID]*PeerData) {
 	f, err := os.Create(filePath)
 	defer f.Close()
 
@@ -151,37 +143,31 @@ func OutputData(filePath, jsonFile string, peerMap map[peer.ID]map[peer.ID]struc
 		}
 	}
 	f.WriteString("\n}")
-}
 
-type queryResult struct {
-	peer peer.ID
-	data map[peer.ID]*peer.AddrInfo
-	err  error
-}
 
-func queryPeer(ctx context.Context, host host.Host, dhtRPC *kaddht.ProtocolMessenger, nextPeer peer.ID) *queryResult {
-	tmpRT, err := kbucket.NewRoutingTable(20, kbucket.ConvertPeerID(nextPeer), time.Hour, host.Peerstore(), time.Hour)
+	f2, err := os.Create(jsonFile)
+	defer f2.Close()
+
 	if err != nil {
-		fmt.Printf("error creating rt for peer %v : %v\n", nextPeer, err)
-		return &queryResult{nextPeer, nil, err}
+		panic(err)
 	}
 
-	localPeers := make(map[peer.ID]*peer.AddrInfo)
-	for i := 0; i < 12; i++ {
-		generatePeer, err := tmpRT.GenRandPeerID(uint(i))
-		if err != nil {
-			panic(err)
-		}
-		peers, err := dhtRPC.GetClosestPeers(ctx, nextPeer, generatePeer)
-		if err != nil {
-			fmt.Printf("error finding data on peer %v with cpl %d : %v\n", nextPeer, i, err)
-			break
-		}
-		for _, ai := range peers {
-			if _, ok := localPeers[ai.ID]; !ok {
-				localPeers[ai.ID] = ai
+	for p := range peerMap {
+		if v, ok := pd[p]; !ok {
+			panic("?")
+		} else {
+			if !v.Success {
+				panic("hoowww")
 			}
 		}
 	}
-	return &queryResult{nextPeer, localPeers, nil}
+
+	for _, d := range pd {
+		b, err := json.Marshal(d)
+		if err != nil {
+			panic(err)
+		}
+		f2.Write(b)
+		f2.WriteString("\n")
+	}
 }
